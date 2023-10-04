37023fdeae5833318ae8d96c934683b48439c88a
종혁 <gw1614@naver.com>
2023-10-04 04:08:01 +0000

    1

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -43,5 +43,23 @@ public class ProductRepository
     {
 		return listOfProducts;
 	}
+    
+    
+    public Product getProductById(String productld)
+    {
+        Product productById = null;
+        
+        for(int i = 0; i<listOfProducts.size(); i++)
+        {
+            Product product = listOfProducts.get(i);
+            if(product != null && product.getProductId() != null && product.getProductId().equals(productId))
+            {
+                productById = product;
+                break;
+            }
+        }
+        return productById;
+    }
 
+    
 }
